08720539a373291fb3cca04c55684e9457a0f90f
matsu1730 <alf.matsuoka2000@gmail.com>
2022-12-22 21:52:58 -0300

    finalizando colisão da bala

--- Assets/Scripts/Bala.cs
+++ Assets/Scripts/Bala.cs
@@ -10,4 +10,14 @@ public class Bala : MonoBehaviour
         //fazendo a bala ir para frente
         GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * velocidade * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider objetoDeColisao)
+    {
+        if(objetoDeColisao.tag == "Zumbi" || objetoDeColisao.gameObject.layer.Equals("Zumbi")) {
+            //Destroy(objetoDeColisao.gameObject);
+            Object.Destroy(objetoDeColisao.gameObject);
+        }
+
+        Destroy(this.gameObject);
+    }
 }
